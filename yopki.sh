157cdade042cb30d5ada33deffd47fadd282eb75
Lucca Romanelli <ldpromanelli@gmail.com>
2025-11-17 11:49:44 -0300

    fix startnash path

--- yopki.sh
+++ yopki.sh
@@ -13,6 +13,6 @@ sleep 0.8
 hyprctl dispatch workspace 5
 sleep 0.5
 
-uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/yopki/trip-planner-backend && docker ps -q | xargs -r docker stop && bash start-nats.sh && docker compose up -d && pnpm run dev && exec zsh' &
+uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/yopki/trip-planner-backend && docker ps -q | xargs -r docker stop && bash ./start-nats.sh && docker compose up -d && pnpm run dev && exec zsh' &
 
 uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/yopki/trip-planner-web/ && npm run dev && exec zsh' &
