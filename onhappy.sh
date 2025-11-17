7e857637d679d519b16ef319bbf4186d18eac50a
Lucca Romanelli <ldpromanelli@gmail.com>
2025-11-17 12:01:38 -0300

    fix dev scripts

--- onhappy.sh
+++ onhappy.sh
@@ -13,7 +13,7 @@ sleep 0.8
 hyprctl dispatch workspace 5
 sleep 0.5
 
-uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/onhappy/onhappy-backend/ && docker ps -q | xargs -r docker stop && docker-compose up -d && sdev; exec zsh' &
+uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/onhappy/onhappy-backend/ && docker ps -q | xargs -r docker stop && docker start $(docker ps -aq -f "name=^onhappy-") && sdev; exec zsh' &
 sleep 0.8
 
 uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/onhappy/onhappy-frontend/ && npm run dev; exec zsh' &
