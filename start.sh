9caa0b0c3bdb0e73cb06c3ee8d17f8cf45f1c7c9
Lucca Romanelli <ldpromanelli@gmail.com>
2025-11-17 11:49:12 -0300

    fix start

--- start.sh
+++ start.sh
@@ -10,10 +10,6 @@ sleep 0.5
 omarchy-launch-browser &
 sleep 1.2
 
-hyprctl dispatch layoutmsg "master"
-sleep 0.5
-hyprctl dispatch layoutmsg "orientationleft"
-sleep 0.5
 hyprctl dispatch workspace 6
 sleep 1.2
 
@@ -26,15 +22,12 @@ sleep 0.8
 omarchy-launch-or-focus-webapp 'Google Chat' 'https://mail.google.com/chat/u/2/#chat/home' &
 sleep 0.8
 
-hyprctl dispatch layoutmsg "magic"
-sleep 0.5
-
 hyprctl dispatch workspace 7
 sleep 0.5
 
-omarchy-launch-webapp Jira 'https://onhappy.atlassian.net/jira/software/c/projects/CHEER/boards/3'
+omarchy-launch-webapp 'https://onhappy.atlassian.net/jira/software/c/projects/CHEER/boards/3'
 sleep 0.5
-omarchy-launch-webapp ClickUp 'https://app.clickup.com/'
+omarchy-launch-webapp 'https://app.clickup.com/'
 sleep 0.8
 omarchy-launch-webapp \
   'https://docs.google.com/spreadsheets/d/1eBNz9AqBo1jv5pLwOGyTwKSk6K328TTa37g5Dt19fwI/edit?pli=1&gid=256386999#gid=256386999' &
@@ -43,38 +36,23 @@ sleep 1.2
 hyprctl dispatch workspace 8
 sleep 0.5
 
-omarchy-launch-webapp TodoIst 'https://app.todoist.com/app/today' &
+omarchy-launch-webapp 'https://app.todoist.com/app/today' &
 sleep 1.0
 
-hyprctl dispatch split:right
-sleep 0.2
-
-omarchy-launch-webapp 'Google Calendar' 'https://calendar.google.com/calendar/u/0/r' &
+omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
 sleep 1.0
 
-hyprctl dispatch cyclenext
-sleep 0.2
-hyprctl dispatch split:bottom
-sleep 0.2
-
-omarchy-launch-webapp 'Google Calendar' 'https://calendar.google.com/calendar/u/0/r' &
+omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
 sleep 1.0
 
-hyprctl dispatch cyclenext
-sleep 0.2
-hyprctl dispatch split:bottom
-sleep 0.2
-
-omarchy-launch-webapp 'Google Calendar' 'https://calendar.google.com/calendar/u/0/r' &
+omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
 sleep 1.0
 
-hyprctl dispatch layoutmsg "magic"
-sleep 0.2
 
 hyprctl dispatch workspace 9
 sleep 0.5
 
-omarchy-launch-webapp YouTubeMusic 'https://music.youtube.com/' &
+omarchy-launch-webapp 'https://music.youtube.com/' &
 sleep 0.8
 
 hyprctl dispatch workspace 2
--- yopki.sh
+++ yopki.sh
@@ -13,18 +13,6 @@ sleep 0.8
 hyprctl dispatch workspace 5
 sleep 0.5
 
-uwsm app -- "$TERMINAL" -e zsh -c '
-  cd ~/yopki/trip-planner-backend &&
+uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/yopki/trip-planner-backend && docker ps -q | xargs -r docker stop && bash start-nats.sh && docker compose up -d && pnpm run dev && exec zsh' &
 
-  docker ps -q | xargs -r docker stop &&
-
-  docker run -d --name trip-planner-bff-nats \
-    -p 4222:4222 -p 8222:8222 -p 6222:6222 \
-    nats -js -m 8222 &&
-
-  docker compose up -d &&
-
-  pnpm run dev &&
-
-  exec zsh
-' &
+uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/yopki/trip-planner-web/ && npm run dev && exec zsh' &
