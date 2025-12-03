d8f7034796c03c8a99c55e45ce42c61bbff5eba0
Lucca Romanelli <ldpromanelli@gmail.com>
2025-12-03 16:46:09 -0300

    yopki

--- yopki.sh
+++ yopki.sh
@@ -1,20 +1,25 @@
+#!/usr/bin/env bash
+
+# workspace 3: frontend web no nvim
 hyprctl dispatch workspace 3
 sleep 0.5
 
-uwsm app -- "$TERMINAL" -e tmux new-session -A -s yopki-front 'cd ~/yopki/trip-planner-web && nvim .; exec zsh' &
+uwsm app -- "$TERMINAL" --command zsh -ic 'cd ~/yopki/trip-planner-web && nvim .; exec zsh' &
 sleep 0.8
 
+# workspace 4: backend no nvim
 hyprctl dispatch workspace 4
 sleep 0.5
 
-uwsm app -- "$TERMINAL" -e tmux new-session -A -s yopki-back 'cd ~/yopki/trip-planner-backend && nvim .; exec zsh' &
+uwsm app -- "$TERMINAL" --command zsh -ic 'cd ~/yopki/trip-planner-backend && nvim .; exec zsh' &
 sleep 0.8
 
+# workspace 5: docker + backend dev + frontend dev + core build:watch
 hyprctl dispatch workspace 5
 sleep 0.5
 
-uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/yopki/trip-planner-backend && docker ps -q | xargs -r docker stop && docker start $(docker ps -aq -f "name=^trip-") && pnpm run dev; exec zsh' &
+uwsm app -- "$TERMINAL" --command zsh -ic 'cd ~/yopki/trip-planner-backend && docker ps -q | xargs -r docker stop && docker start $(docker ps -aq -f "name=^trip-") && pnpm run dev; exec zsh' &
 
-uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/yopki/trip-planner-web/ && npm run dev; exec zsh' &
+uwsm app -- "$TERMINAL" --command zsh -ic 'cd ~/yopki/trip-planner-web/ && npm run dev; exec zsh' &
 
-uwsm app -- "$TERMINAL" -e zsh -c 'cd ~/yopki/trip-planner-web/packages/core/ && npm run build:watch; exec zsh' &
+uwsm app -- "$TERMINAL" --command zsh -ic 'cd ~/yopki/trip-planner-web/packages/core/ && npm run build:watch; exec zsh' &
