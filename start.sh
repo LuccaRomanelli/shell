48b4eb3173faa91d77f97eb4ef973205d72d5928
Lucca Romanelli <ldpromanelli@gmail.com>
2025-12-17 14:34:08 -0300

    imporove

--- start.sh
+++ start.sh
@@ -1,61 +1,48 @@
 sleep 0.2
 hyprctl dispatch workspace 1
-sleep 0.5
+sleep 0.8
 
 omarchy-launch-webapp 'https://apps.abacus.ai/chatllm/' &
 sleep 0.5
 omarchy-launch-webapp 'https://youtube.com/' &
-sleep 1.2
+sleep 2
 
 hyprctl dispatch workspace 2
-sleep 0.5
-
+sleep 1
 omarchy-launch-browser &
-sleep 1.2
+sleep 1
 
 hyprctl dispatch workspace 6
-sleep 1.2
+sleep 0.8
 
 omarchy-launch-or-focus-webapp WhatsApp 'https://web.whatsapp.com/' &
-sleep 0.8
-
+sleep 0.5
 omarchy-launch-or-focus-webapp ClickUp 'https://app.clickup.com/' &
-sleep 0.8
-
-omarchy-launch-or-focus-webapp 'Google Chat' 'https://mail.google.com/chat/u/2/#chat/home' &
-sleep 0.8
+sleep 0.5
+omarchy-launch-or-focus-webapp 'Google Chat' 'https://mail.google.com/chat/u/3/?pli=1#chat/home' &
+sleep 0.5
 
 hyprctl dispatch workspace 7
-sleep 0.5
+sleep 0.8
 
 omarchy-launch-webapp 'https://onhappy.atlassian.net/jira/software/c/projects/CHEER/boards/3'
 sleep 0.5
 omarchy-launch-webapp 'https://app.clickup.com/'
-sleep 0.8
+sleep 0.5
 omarchy-launch-webapp \
   'https://docs.google.com/spreadsheets/d/1eBNz9AqBo1jv5pLwOGyTwKSk6K328TTa37g5Dt19fwI/edit?pli=1&gid=256386999#gid=256386999' &
-sleep 1.2
+sleep 0.5
 
 hyprctl dispatch workspace 8
-sleep 0.5
+sleep 0.8
 
 omarchy-launch-webapp 'https://app.todoist.com/app/today' &
-sleep 1.0
-
-omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
-sleep 1.0
-
-omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
-sleep 1.0
-
-omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
-sleep 1.0
-
-
-hyprctl dispatch workspace 9
 sleep 0.5
-
-omarchy-launch-webapp 'https://music.youtube.com/' &
+omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
+sleep 0.5
+omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
+sleep 0.5
+omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
 sleep 0.8
 
 hyprctl dispatch workspace 2
