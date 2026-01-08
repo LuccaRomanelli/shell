753e72544e170a5f607cc9a9518df9114b3a6abf
Lucca Romanelli <ldpromanelli@gmail.com>
2026-01-08 15:39:29 -0300

    .

--- start.sh
+++ start.sh
@@ -20,30 +20,14 @@ omarchy-launch-or-focus-webapp WhatsApp 'https://web.whatsapp.com/' &
 sleep 0.5
 omarchy-launch-or-focus-webapp ClickUp 'https://app.clickup.com/' &
 sleep 0.5
-omarchy-launch-or-focus-webapp 'Google Chat' 'https://mail.google.com/chat/u/3/?pli=1#chat/home' &
-sleep 0.5
 
 hyprctl dispatch workspace 7
 sleep 0.8
 
-omarchy-launch-webapp 'https://onhappy.atlassian.net/jira/software/c/projects/CHEER/boards/3'
-sleep 0.5
 omarchy-launch-webapp 'https://app.clickup.com/'
 sleep 0.5
 omarchy-launch-webapp \
   'https://docs.google.com/spreadsheets/d/1eBNz9AqBo1jv5pLwOGyTwKSk6K328TTa37g5Dt19fwI/edit?pli=1&gid=256386999#gid=256386999' &
 sleep 0.5
-
-hyprctl dispatch workspace 8
-sleep 0.8
-
-omarchy-launch-webapp 'https://app.todoist.com/app/today' &
-sleep 0.5
-omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
-sleep 0.5
-omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
-sleep 0.5
-omarchy-launch-webapp 'https://calendar.google.com/calendar/u/0/r' &
-sleep 0.8
 
 hyprctl dispatch workspace 2
